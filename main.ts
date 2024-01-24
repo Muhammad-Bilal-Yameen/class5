0e08c612350190a0d8f94021aedda03eecbb5dc2
muhammad bilal yameen <bilalyameen03151190023@gmail.com>
2024-01-24 00:38:35 -0800

    class5

---
+++ main.ts
@@ -0,0 +1,145 @@
+// FoorLoop
+
+// syntax
+// for (){}
+
+
+// Exercsie # 1
+
+// let n1 = 2
+
+// for(let a = 1; a <= 10; a ++  ){
+//     if(n1*a ==10){}
+// console.log(`${n1} * ${a} = ${n1*a}`);
+
+// }
+// // ans = table of 2 completed
+
+
+
+
+
+
+// Exercsie # 2
+
+
+
+// for(let a = 1; a <= 10; a++){
+
+//     let ans = a * 2
+//     console.log("2", "x", "a", "=", ans);
+    
+// }
+
+
+// ans = table of 2 completed
+
+
+// Exercsie # 3
+
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+
+// let arr = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j"]
+// for (let index =0; index<=10; index++) {
+//     console.log(arr);
+    
+// }
+
+// Exercsie # 4
+
+// for(let i =1; i<=25; i++){
+//     console.log(i);
+    
+// }
+
+// ans is 1 to 25 quantity
+
+
+
+// Exercsie # 5
+
+// let n1 = 2
+// for (let a =1; a<=10; a++){
+//     if(n1*a == 10){
+//     console.log(`${n1} * ${a} = ${n1*a}`);
+// break
+// }
+    
+// }
+
+// ans = 2 x = 10  
+
+
+
+
+
+// Exercsie # 6
+
+let vedios 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for( let i = 1; i <= 12; i++ ){
+//     console.log(i);
+// }
+
+
+
+
+
+// for ( let a = 1; a <=10; a++ ){
+//     let ans = a *2
+//     console.log("2","x",a,"=", ans )
+// }
+
+
+
+
+
+// Example
+
+// for( let b = 0; b < 3; b++ ){
+//     console.log("Hello world")
+// }
+
+
+// Example 2
+
+// for( let i = 0; i < 3; i++ ){
+//     console.log("hello world" + i
